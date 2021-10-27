abd02e2574162a49ce7f9efb514219025c6d27ba
jsuis <76794094+jsuis@users.noreply.github.com>
2021-10-27 16:30:11 +0100

    Attempt to resolve a config issue. FileNotFoundError: [Errno 2] No such file or directory

--- bot/datastoragefunctions.py
+++ bot/datastoragefunctions.py
@@ -1,24 +1,29 @@
 import json
+import os
+
+DSF_PATH = os.path.join(os.path.dirname(__file__))
+DATA_STORAGE = os.path.join(DSF_PATH, "data_storage.json")
+USER_STORAGE = os.path.join(DSF_PATH, "user_storage.json")
 
 def dataStorageRead():
     """
     Simple data_storage.json read (example of data_storage.json in README)
     """
-    with open("bot/data_storage.json", "r") as file:
+    with open(DATA_STORAGE, "r") as file:
         return json.load(file)
     
 def dataStorageWrite(data):
     """
     Data storage writer, accepts only a whole data file, please do not send partial data
     """
-    with open("bot/data_storage.json", "w") as file:
+    with open(DATA_STORAGE, "w") as file:
         json.dump(data, file)
 
 def userStorage(wr, user, data = None):
     """
     All in one user_storage.json reader
     """
-    with open("bot/user_storage.json", "r") as file:
+    with open(USER_STORAGE, "r") as file:
         all_users = json.load(file)
     if wr == "r":
         try:
@@ -26,6 +31,6 @@ def userStorage(wr, user, data = None):
         except:
             return all_users["default"]
     elif wr == "w":
-        with open("bot/user_storage.json", "w") as file:
+        with open(USER_STORAGE, "w") as file:
             all_users[str(user)] = data
             json.dump(all_users, file)
